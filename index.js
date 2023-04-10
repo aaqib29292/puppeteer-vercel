ae6f3f50401fee60f2d5506da3b2ad96fdf58072
Aaqib Parvez <aaqib29292@gmail.com>
2023-04-10 13:01:04 +0530

    add receipt code

--- index.js
+++ index.js
@@ -24,11 +24,28 @@ app.get("/api", async (req, res) => {
   }
 
   try {
+    const domain = 'https://finschoolb.dev.newtonclassroom.com'
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaXNzaW9uT2ZmaWNlciIsImVtYWlsIjoicHJvLmFkbUBnZWR1LmRlbW8uc3VycmVhbC5jb21wYW55Iiwic2Nob29sSWQiOjE3LCJzY2hvb2xDb2RlIjoiZmluc2Nob29sYiIsImlhdCI6MTY3NjI4Njk0Mn0.5ZAzo7p7jnrxSHzDawub5tcmBC4waExoJLhXueM8wDo'
+
     let browser = await puppeteer.launch(options);
 
-    let page = await browser.newPage();
-    await page.goto("https://www.google.com");
-    res.send(await page.title());
+    const page = await browser.newPage();
+    await page.goto(`${domain}/login?status=success&jwt=${token}&role=admin`, {
+      waitUntil: 'networkidle2',
+    })
+    await page.goto(`${domain}/no-menu/receipts/fees`, {
+      waitUntil: 'networkidle2',
+    });
+    // page.pdf() is currently supported only in headless mode.
+    // @see https://bugs.chromium.org/p/chromium/issues/detail?id=753118
+    const buffer = await page.pdf({
+      format: 'A4',
+    });
+
+    // await browser.close();
+
+    res.set('Content-Type', 'application/pdf')
+    res.status(201).send(Buffer.from(buffer, "binary"))
   } catch (err) {
     console.error(err);
     return null;
